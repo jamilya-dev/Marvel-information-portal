848238fcd6349c47bd6d43f4ab448ee790cd7b1a
jamilya-dev <omg.groups@yandex.ru>
2025-02-23 18:38:16 +0300

    remake

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import Spinner from '../spinner/Spinner';
@@ -6,62 +6,50 @@ import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
 import './charList.scss';
 
-class CharList extends Component {
-  state = {
-    charList: [],
-    loading: true,
-    error: false,
-    newItemLoading: false,
-    offset: 210,
-    charEnded: false,
+const CharList = (props) => {
+  const [charList, setCarList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [newItemLoading, setNewItemLoading] = useState(false);
+  const [offset, setOffset] = useState(210);
+  const [charEnded, setCharEnded] = useState(false);
+
+  const marvelService = new MarvelService();
+
+  useEffect(() => {
+    onRequest()
+  }, [])
+
+  const onRequest = (offset) => {
+    onCharListLoading();
+    marvelService.getAllCharacters(offset).then(onCharListLoaded).catch(onError);
   };
 
-  marvelService = new MarvelService();
-
-  componentDidMount() {
-    this.onRequest();
-  }
-
-  onRequest = (offset) => {
-    this.onCharListLoading();
-    this.marvelService.getAllCharacters(offset).then(this.onCharListLoaded).catch(this.onError);
+  const onCharListLoading = () => {
+    setNewItemLoading(true)
   };
 
-  onCharListLoading = () => {
-    this.setState({
-      newItemLoading: true,
-    });
-  };
-
-  onCharListLoaded = (newCharList) => {
+  const onCharListLoaded = (newCharList) => {
     let ended = false;
     if (newCharList.length < 9) {
       ended = true;
     }
-    this.setState(({ offset, charList }) => ({
-      charList: [...charList, ...newCharList],
-      loading: false,
-      newItemLoading: false,
-      offset: offset + 9,
-      charEnded: ended,
-    }));
+
+    setCarList(charList => [...charList, ...newCharList])
+    setLoading(false)
+    setNewItemLoading(newItemLoading => false)
+    setOffset(offset => offset + 9)
+    setCharEnded(charEnded => ended)
   };
 
-  onError = () => {
-    this.setState({
-      error: true,
-      loading: false,
-    });
+  const onError = () => {
+    setError(error => true)
+    setLoading(loading => false)
   };
 
+  const itemRefs = useRef([]);
 
-  itemRefs = [];
-
-  setRef = (ref) => {
-    this.itemRefs.push(ref);
-  }
-
-  focusOnItem = (id) => {
+  const focusOnItem = (id) => {
     // Я реализовал вариант чуть сложнее, и с классом и с фокусом
     // Но в теории можно оставить только фокус, и его в стилях использовать вместо класса
     // На самом деле, решение с css-классом можно сделать, вынеся персонажа
@@ -69,16 +57,14 @@ class CharList extends Component {
     // и не факт, что мы выиграем по оптимизации за счет бОльшего кол-ва элементов
 
     // По возможности, не злоупотребляйте рефами, только в крайних случаях
-    this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
-    this.itemRefs[id].classList.add('char__item_selected');
-    this.itemRefs[id].focus();
+    itemRefs.current.forEach(item => item.classList.remove('char__item_selected'));
+    itemRefs.current[id].classList.add('char__item_selected');
+    itemRefs.current[id].focus();
   }
-
-
 
   // Этот метод создан для оптимизации,
   // чтобы не помещать такую конструкцию в метод render
-  renderItems(arr) {
+  function renderItems(arr) {
     const items = arr.map((item, i) => {
       let imgStyle = { objectFit: 'cover' };
       if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
@@ -89,16 +75,16 @@ class CharList extends Component {
         <li
           tabIndex={0}
           className="char__item"
-          ref={this.setRef}
+          ref={el => itemRefs.current[i] = el}
           key={item.id}
           onClick={() => {
-            this.props.onCharSelected(item.id);
-            this.focusOnItem(i);
+            props.onCharSelected(item.id);
+            focusOnItem(i);
           }}
           onKeyPress={(e) => {
             if (e.key === ' ' || e.key === "Enter") {
-              this.props.onCharSelected(item.id);
-              this.focusOnItem(i);
+              props.onCharSelected(item.id);
+              focusOnItem(i);
             }
           }}>
           <img src={item.thumbnail} alt={item.name} style={imgStyle} />
@@ -112,28 +98,25 @@ class CharList extends Component {
     return <ul className="char__grid">{items}</ul>;
   }
 
-  render() {
-    const { charList, loading, error, offset, newItemLoading, charEnded } = this.state;
+  const items = renderItems(charList);
 
-    const items = this.renderItems(charList);
+  const errorMessage = error ? <ErrorMessage /> : null;
+  const spinner = loading ? <Spinner /> : null;
+  const content = !(loading || error) ? items : null;
 
-    const errorMessage = error ? <ErrorMessage /> : null;
-    const spinner = loading ? <Spinner /> : null;
-    const content = !(loading || error) ? items : null;
+  return (
+    <div className="char__list">
+      {errorMessage}
+      {spinner}
+      {content}
+      <button disabled={newItemLoading} onClick={() => onRequest(offset)} className="button button__main button__long" style={{ display: charEnded ? 'none' : 'block' }}>
+        <div className="inner">load more</div>
+      </button>
+    </div>
+  );
 
-    return (
-      <div className="char__list">
-        {errorMessage}
-        {spinner}
-        {content}
-        <button disabled={newItemLoading} onClick={() => this.onRequest(offset)} className="button button__main button__long" style={{ display: charEnded ? 'none' : 'block' }}>
-          <div className="inner">load more</div>
-        </button>
-      </div>
-    );
-  }
 }
 CharList.propTypes = {
-  onCharSelected: PropTypes.func,
+  onCharSelected: PropTypes.func.isRequired,
 };
 export default CharList;
